9e9135cbd03c695f867fce1fbaf1e0261d70fc4f
张啸 <petsgre@gmail.com>
2020-03-19 09:21:59 +0800

    feat: update init.el

--- notes/init.el
+++ notes/init.el
@@ -8,6 +8,13 @@
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
+
+;; 将询问语改成 y和n简写
+(fset yes-or-no-p 'y-or-n-p)
+
+;; 使用f5刷新
+(global-set-key (kbd "<f5>") 'revert-buffer)
+
 ;; try可以临时使用这个包，再次重启emacs会删除掉使用try安装的包
 (use-package try
   :ensure t)
@@ -23,3 +30,49 @@
 ;;(defalias 'list-buffers' ibuffer)
 ;; 在当前窗口显示buffer list
 (global-set-key (kbd "C-x C-b") 'ibuffer)
+
+
+(use-package counsel
+  :ensure t
+  )
+;; 使用swiper在底部显示待选项，可以用 C n C p快速切换
+(use-package swiper
+  :ensure t
+  :config
+  (progn
+    (ivy-mode 1)
+    (setq ivy-use-virtual-buffers t)
+    (setq enable-recursive-minibuffers t)
+    ;; enable this if you want `swiper' to use it
+    ;; (setq search-default-mode #'char-fold-to-regexp)
+    (global-set-key "\C-s" 'swiper)
+    (global-set-key (kbd "C-c C-r") 'ivy-resume)
+    (global-set-key (kbd "<f6>") 'ivy-resume)
+    (global-set-key (kbd "M-x") 'counsel-M-x)
+    (global-set-key (kbd "C-x C-f") 'counsel-find-file)
+    (global-set-key (kbd "<f1> f") 'counsel-describe-function)
+    (global-set-key (kbd "<f1> v") 'counsel-describe-variable)
+    (global-set-key (kbd "<f1> l") 'counsel-find-library)
+    (global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
+    (global-set-key (kbd "<f2> u") 'counsel-unicode-char)
+    (global-set-key (kbd "C-c g") 'counsel-git)
+    (global-set-key (kbd "C-c j") 'counsel-git-grep)
+    (global-set-key (kbd "C-c k") 'counsel-ag)
+    (global-set-key (kbd "C-x l") 'counsel-locate)
+    (global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
+    (define-key minibuffer-local-map (kbd "C-r") 'counsel-minibuffer-history)
+    ))
+;; 自动完成
+(use-package auto-complete
+  :ensure t
+  :init
+  (progn
+    (ac-config-default)
+    (global-auto-complete-mode t)
+    ))
+
+(use-package zenburn-theme
+  :ensure t
+  :config (load-theme 'zenburn t))
+;; 设置字体格式 describe-font 获取描述文件
+(set-default-font "-*-Monaco-normal-normal-normal-*-16-*-*-*-m-0-iso10646-1")
